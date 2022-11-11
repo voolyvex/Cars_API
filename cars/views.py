135d2dfb96af473dc29d9222fd3389846eac932a
voolyvex <1joeknapp@gmail.com>
2022-11-11 15:17:31 -0600

    Created the functionality to update a specific car in the cars app views file

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,14 +17,16 @@ def cars_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def car_detail(request, pk):
-    try:
-        car = Car.objects.get(pk=pk)
-        serializer = CarSerializer(car)
-        return Response(serializer.data)
+    car = get_object_or_404(Car, pk=pk)
+    if request.method == 'GET':
         
-    except Car.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-
+        serializer = CarSerializer(car);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        
+        serializer = CarSerializer(car, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
